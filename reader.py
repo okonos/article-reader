4a6351166d880ff55ab29dcd685be35267677dd0
Okonos <tepyzboj@gmail.com>
2018-01-11 16:57:41 +0100

    Implement articles parsing and converting to one big file

--- reader.py
+++ reader.py
@@ -2,6 +2,7 @@ import boto3
 import hashlib
 from flask import Flask, render_template, request, redirect
 from flask_s3 import FlaskS3
+from newspaper import Article
 
 
 app = Flask(__name__)
@@ -17,16 +18,33 @@ polly = boto3.client('polly')
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        text = request.form['text']
-        response = polly.synthesize_speech(
-            OutputFormat='mp3',
-            Text=text,
-            TextType='text',  # 'ssml',
-            VoiceId='Joanna'
-        )
-        filename = hashlib.md5(text.encode('utf-8')).hexdigest() + '.mp3'
-        s3.put_object(Bucket='reader-app-bucket', Key=filename,
-                      Body=response['AudioStream'].read(),
+        voice = request.form['voiceSelect']
+        # text = request.form['text']
+        url = request.form['url']
+        a = Article(url)
+        a.download()
+        a.parse()
+        text = a.text
+        # TODO ssml
+        # text = a.text.replace('\n\n',
+
+        speech = b''
+        chunkSize = 1200
+        while text:
+            i = text[chunkSize:].find('.') + 1
+            chunk = text[:chunkSize+i]
+            text = text[chunkSize+i:].strip()
+            response = polly.synthesize_speech(
+                OutputFormat='mp3',
+                Text=chunk,
+                TextType='text',  # 'ssml',
+                VoiceId=voice
+            )
+            speech += response['AudioStream'].read()
+
+        filename = hashlib.md5(url.encode('utf-8')).hexdigest() + '.mp3'
+        s3.put_object(Bucket=BUCKET_NAME, Key=filename,
+                      Body=speech,  # response['AudioStream'].read(),
                       ContentType='audio/mpeg',
                       ACL='public-read')
         return redirect(BUCKET_URL + filename)
